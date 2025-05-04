5de11557ec2238ceb93c27f9d75143b8ea1b7880
haruuusame <141504902+haruuusame@users.noreply.github.com>
2025-05-04 05:31:58 +0000

    ConsoleUtilを追加

--- src/util/ConsoleUtil.java
+++ src/util/ConsoleUtil.java
@@ -1,13 +1,18 @@
 package util;
 
 import java.util.Scanner;
-
+/**
+ * CLIの共通処理を管理するクラス
+ */
 public class ConsoleUtil {
+
+    // 画面をクリアする(UNIX系で有効)
     public static void clearScreen() {
         System.out.print("\033[H\033[2J");
         System.out.flush();
     }
 
+    // 数字の入力のみを受け付け、正しい値が入力されるまで繰り返す
     public static int safeIntInput(String prompt,Scanner scanner) {
         while (true) {
             System.out.print(prompt);
@@ -17,8 +22,23 @@ public class ConsoleUtil {
                 return value;
             } else {
                 scanner.nextLine();
-                System.out.println("[エラー] 無効な数字です。再入力してください。");
+                error_view("無効な数字です。再入力してください。");
             }
         }
     }
+
+    // タイトルフォーマットで文章を描画。引数は可変長
+    public static void title_view(String... title){
+        System.out.println("+------------------------------------------------------+");
+        for(String t:title){
+            System.out.printf("| %-50s |\n", t);
+        }
+        System.out.println("+------------------------------------------------------+");
+    }
+
+    // エラーを表示
+    public static void error_view(String message){
+        System.out.println("[エラー]" + message);
+    }
+
 }
