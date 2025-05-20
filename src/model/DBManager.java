6d75700516d05ae763f7daee8b2db648eb3c6602
haruuusame <141504902+haruuusame@users.noreply.github.com>
2025-05-20 14:20:18 +0900

    base

---
+++ src/model/DBManager.java
@@ -0,0 +1,47 @@
+import java.sql.*;
+public class DBManager{
+    private String URL;
+    Connection con = null;
+    DBManager(){
+        String baseDir = System.getProperty("user.dir");
+        URL = "jdbc:sqlite:" + baseDir + "/data/Menu.db";
+    }
+    public void connect(){
+        try{
+            con = DriverManager.getConnection(URL);
+            System.out.println("接続成功");
+        }catch(SQLException e) {
+            System.err.println("[接続エラー] " + e.getMessage());
+        }
+        
+    }
+    public void disconnect(){
+        if(con != null){
+            try{
+                con.close();
+            }catch(SQLException e){
+                System.err.println("[接続エラー] " + e.getMessage());
+            }
+        }
+    }
+    public MenuCatalog createMenuCatalogAll() {
+        List<Menu> menus = new ArrayList<>();
+        try(
+            PreparedStatement pstmt = con.prepareStatement("SELECT * FROM menu;"),
+            ResultSet rs = pstmt.executeQuery()
+        ){
+            while(rs.next()){
+                int itemId = rs.getInt("itemId");
+                String itemName = rs.getString("itemName");
+                int price = rs.getInt("price");
+                int stockQuantity = rs.getInt("stockQuantity");
+                String category = rs.getString("category");
+                menus.add(new Menu(itemId,itemName,price,stockQuantity,category));
+            }
+        }catch(SQLException e) {
+            System.err.println("[エラー] " + e.getMessage());
+        }
+        MenuCatalog catalog = new MenuCatalog(menus);
+        return catalog;
+    }
+}
