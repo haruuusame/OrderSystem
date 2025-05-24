91147e23e672f1f5e176788a439c8f0ec1813cdb
TasukuKohama <tasupokemo@gmail.com>
2025-05-25 01:49:48 +0900

    update

--- src/Employee.java
+++ src/Employee.java
@@ -1,12 +1,12 @@
+import java.util.ArrayList;
+import model.Menu;
 import model.Order;
 import model.OrderLine;
-import model.Menu;
-import java.*;
 
 public class Employee {
-    ArrayList itemNameList = new ArrayList<String>();
-    ArrayList itemPriceList = new ArrayList<Integer>();
-    ArrayList itemQuantityList = new ArrayList<Integer>();
+    ArrayList<String> itemNameList = new ArrayList<>();
+    ArrayList<Integer> itemPriceList = new ArrayList<>();
+    ArrayList<Integer> itemQuantityList = new ArrayList<>();
     int totalSales = 0;
 
     //注文内容を確認するメソッド
@@ -27,7 +27,7 @@ public class Employee {
     }
 
     //注文履歴を更新するメソッド
-    public static void updateOrderHistory(Order order) {
+    public static void updateOrderHistory(Order order, Employee employee) {
         int totalPrice = order.calculateTotalPrice();
 
         for (OrderLine line : order.asList()) {
@@ -35,22 +35,22 @@ public class Employee {
             int itemPrice = line.getMenu().getPrice();
             int itemQuantity = line.getQuantity(); 
             
-            itemNameList.add(itemName);
-            itemPriceList.add(itemPrice);
-            itemQuantityList.add(itemQuantity);
+            employee.itemNameList.add(itemName);
+            employee.itemPriceList.add(itemPrice);
+            employee.itemQuantityList.add(itemQuantity);
         }
 
-        int n = itemNameList.size();
-        totalSales += totalPrice;
+        int n = employee.itemNameList.size();
+        employee.totalSales += totalPrice;
         System.out.println("注文履歴");
         System.out.printf("お客様番号：%d\n", order.getOrderId());
         System.out.printf("注文日時：" + order.getOrderDate());
         for (int i = 0; i < n; i++) {
             System.out.printf("%s | %d | %d\n", 
-            itemNameList.get(i), itemPriceList.get(i), itemQuantityList.get(i));
+            employee.itemNameList.get(i), employee.itemPriceList.get(i), employee.itemQuantityList.get(i));
         }
         System.out.printf("合計金額：%d\n", totalPrice);
-        System.out.printf("合計売上額：%d\n", totalSales);
+        System.out.printf("合計売上額：%d\n", employee.totalSales);
     }
 
     //在庫の更新を行うメソッド
@@ -59,7 +59,7 @@ public class Employee {
             String itemName = line.getMenu().getItemName();
             int itemQuantity = line.getQuantity();
             if (menu.getStockQuantity() >= itemQuantity) {
-                menu.getStockQuantity() -= itemQuantity;
+                //menu.getStockQuantity() -= itemQuantity;
                 System.out.printf("%sが%d個注文され残りは%d個です。\n",
                 itemName, itemQuantity, menu.getStockQuantity());
             }
