e6b464e0e422477476bcd697ac1375795e21a11e
TasukuKohama <tasupokemo@gmail.com>
2025-05-25 20:07:53 +0900

    update

---
+++ src/Employee.java
@@ -0,0 +1,83 @@
+import java.util.ArrayList;
+import model.Menu;
+import model.Order;
+import model.OrderLine;
+import model.DBManager;
+
+public class Employee {
+    ArrayList<String> itemNameList = new ArrayList<>();
+    ArrayList<Integer> itemPriceList = new ArrayList<>();
+    ArrayList<Integer> itemQuantityList = new ArrayList<>();
+    int totalSales = 0;
+
+    public static void showOrder(int orderId, DBManager dbm) {
+        Optional<Order> opOrder = dbm.fetchOrderById(orderId);
+        if (opOrder.isPresent()) {
+            Order order = opOrder.get();
+            checkOrder(order);
+        } else {
+            System.out.println("注文が見つかりませんでした。");
+        }
+    }
+
+    //注文内容を確認するメソッド
+    public static void checkOrder(Order order) {
+        int totalPrice = order.calculateTotalPrice();
+
+        System.out.println("注文内容確認");
+        System.out.printf("お客様番号：%d\n", order.getOrderId());
+        System.out.println("注文日時：" + order.getOrderDate());
+        System.out.println("商品名 | 金額 | 個数");
+        for (OrderLine line : order.asList()) {
+            String itemName = line.getMenu().getItemName();
+            int itemPrice = line.getMenu().getPrice();
+            int itemQuantity = line.getQuantity();
+            System.out.printf("%s | %d | %d\n", itemName, itemPrice, itemQuantity);
+        }
+        System.out.printf("合計金額：%d\n", totalPrice);
+    }
+
+    //注文履歴を更新するメソッド
+    public static void showOrderHistory(int status, DBManager dbm) {
+        List<Order> orders;
+        if (status == -1) {
+            orders = dbm.fetchOrdersAll();
+        } else if (0 <= status && status <= 3) {
+            orders = dbm.fetchOrdersByStatus(status);
+        } else {
+            System.out.println("不正なステータスです。");
+            return;
+        }
+        for (Order order : orders) {
+            System.out.println("----------------------");
+            checkOrder(order);
+        }
+    }
+
+    //在庫の更新を行うメソッド
+    public static void updateStock(int itemId, int quantity, DBManager dbm) {
+        boolean success = dbm.restockMenuItem(itemId,quantity);
+        if (success) {
+            System.out.printf("商品ID%dの在庫を%d個補充しました。\n", itemId, quantity);
+        } else {
+            System.out.printf("在庫補充できませんでした。(商品ID：%d)\n", itemId);
+        }
+    }
+
+    public static void updateOrderStatus(int orderId, int status, DBManager dbm) {
+        dbm.updateStatusAll(orderId, status);
+        System.out.printf("注文ID%dのステータスを%dに変更しました。\n",orderId, status);
+    }
+
+    public static String convertIntegerIntoString(int status) {
+        String statusString;
+        switch(status) {
+            case 0 : statusString = "処理中"; break;
+            case 1 : statusString = "提供中"; break;
+            case 2 : statusString = "完了"; break;
+            case 3 : statusString = "キャンセル"; break;
+            default : statusString = ""; break;
+        }
+        return statusString;
+    }
+}
